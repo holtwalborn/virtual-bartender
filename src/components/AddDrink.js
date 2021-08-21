61a83d137df9c320f5971d5612e8ff309d1f553d
Holt <holt.walborn@fullstackacademy.com>
2021-08-21 13:08:53 -0400

    removed accidental code

--- src/components/AddDrink.js
+++ src/components/AddDrink.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory, userHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import API from '../utilities/api';
 
